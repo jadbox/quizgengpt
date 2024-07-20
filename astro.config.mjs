4f2ce81d28e9b4696db1a125fdcc253a8c3c9d5a
Jonathan Dunlap <jdunlap@outlook.com>
2024-07-19 22:32:45 -0400

    remove react p2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+// import react from "@astrojs/react"; // avoid using this for now
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
-    }), 
-    react()
-  ]
+    }),
+    // react()
+  ],
 });
