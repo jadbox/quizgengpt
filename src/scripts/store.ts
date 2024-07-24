062dd872df33c64d7f3188988a80089777555d89
Luis Egea <alphasearchseo@gmail.com>
2024-07-24 19:40:06 +0100

    feat: selected goals listed as targets in goals page

--- src/scripts/store.ts
+++ src/scripts/store.ts
@@ -1,5 +1,4 @@
 import { atom } from 'nanostores';
-import { activities } from './globals';
 import type { ActivityLabel } from './globals';
 
-export const isSelected = atom<ActivityLabel[]>([]);
+export const selectedActivities = atom<ActivityLabel[]>([]);
