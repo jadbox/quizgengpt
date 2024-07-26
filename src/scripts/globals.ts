2a8e5119992a4bee686d85fe79f5bb27ef30df1d
Jonathan Dunlap <jdunlap@outlook.com>
2024-07-25 20:02:09 -0400

    chore: update task list + remove edit tasks for now

--- src/scripts/globals.ts
+++ src/scripts/globals.ts
@@ -1,23 +1,22 @@
 export const activities = [
-  { label: "study", icon: "school" },
+  { label: "exercise", icon: "fitness_center" },
+  { label: "meditate", icon: "spa" },
+  { label: "read", icon: "menu_book" },
+  { label: "learn", icon: "school" },
   { label: "work", icon: "work" },
+  { label: "socialize", icon: "people" },
+  { label: "cook", icon: "restaurant" },
+  { label: "clean", icon: "cleaning_services" },
   { label: "relax", icon: "self_improvement" },
-  { label: "family", icon: "family_restroom" },
-  { label: "friends", icon: "people" },
-  { label: "date", icon: "favorite" },
-  { label: "sport", icon: "directions_run" },
-  { label: "party", icon: "celebration" },
-  { label: "movies", icon: "movie" },
-  { label: "reading", icon: "menu_book" },
-  { label: "gaming", icon: "sports_esports" },
-  { label: "shopping", icon: "shopping_cart" },
-  { label: "good meal", icon: "restaurant" },
-  { label: "cleaning", icon: "cleaning_services" },
-  { label: "transit", icon: "directions_transit" },
-  { label: "school", icon: "edit" },
+  { label: "plan", icon: "event" },
+  { label: "create", icon: "create" },
+  { label: "connect", icon: "connect_without_contact" },
+  { label: "reflect", icon: "emoji_objects" },
+  { label: "organize", icon: "format_list_bulleted" },
+  { label: "explore", icon: "explore" },
 ];
 
-export type ActivityLabel = typeof activities[number]['label'];
+export type ActivityLabel = (typeof activities)[number]["label"];
 
 // Note, this is a fake API call, in a real app you would fetch this data from a server to include custom activities
 export async function getActivities() {
