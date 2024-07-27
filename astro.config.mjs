4e267a554ca11b2d7b84a8b97825eacb6b9df4a1
Jonathan Dunlap <jdunlap@outlook.com>
2024-07-26 22:04:27 -0400

    fly deploy config wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://app.thrivetogether.io",
   output: "server",
   integrations: [
     tailwind({
