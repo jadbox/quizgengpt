a7baa892ffda82976b5d474175702d3d0bdeaa31
Jonathan Dunlap <jdunlap@outlook.com>
2024-07-26 22:45:03 -0400

    add https

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 // import react from "@astrojs/react"; // avoid using this for now
-
+import bun from "astro-bun-adapter";
 // https://astro.build/config
 export default defineConfig({
-  site: "http://app.thrivetogether.io",
+  site: "https://app.thrivetogether.ai",
   output: "server",
+  adapter: bun(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
