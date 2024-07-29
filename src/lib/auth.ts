635c16148bc00b3b8c2b5f98236e1a6b2c20b270
Jonathan Dunlap <jdunlap@outlook.com>
2024-07-29 15:42:36 -0400

    fix login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-export function loginCheck(Astro: any) {
+export function loginCheck(Astro:any) {
   const { cookies, redirect } = Astro;
   const accessToken = cookies.get("sb-access-token");
   const refreshToken = cookies.get("sb-refresh-token");
@@ -6,4 +6,10 @@ export function loginCheck(Astro: any) {
   if (!accessToken || !refreshToken) {
     return redirect("/signin");
   }
+
+  if(!cookies.get('uid')) {
+    return redirect('/signin');
+  }
+
+  return null;
 }
