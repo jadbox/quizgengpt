66d6a47dd1dd5b51e1defc53148ce4604fe3d97f
Jonathan Dunlap <jdunlap@outlook.com>
2024-07-30 22:26:22 -0400

    chore: move user to goal tracker page if goal is still open

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -3,6 +3,7 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
+  cookies.delete('uid');
   cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" });
   return redirect("/signin");
