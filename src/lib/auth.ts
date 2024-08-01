9abcc9b398c5fb45749e97cd4635858288b9c164
Jonathan Dunlap <jdunlap@outlook.com>
2024-08-01 09:51:31 -0400

    chore: add rewrite for goal selection and tracking to be the same screen

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,16 @@
-export function loginCheck(Astro:any) {
+export function loginCheck(Astro: any) {
   const { cookies, redirect } = Astro;
   const accessToken = cookies.get("sb-access-token");
   const refreshToken = cookies.get("sb-refresh-token");
 
   if (!accessToken || !refreshToken) {
+    console.log("no auth token");
     return redirect("/signin");
   }
 
-  if(!cookies.get('uid')) {
-    return redirect('/signin');
+  if (!cookies.get("uid")) {
+    console.log("no auth cookie");
+    return redirect("/signin");
   }
 
   return null;
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,10 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../../supabase/supabase";
 import type { Tables } from "../../supabase/supabase";
 
-console.log(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
-);
+// console.log(
+//   import.meta.env.PUBLIC_SUPABASE_URL,
+//   import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+// );
 
 export let messages: Tables<"messages">;
 export const supabase = createClient<Database>(
