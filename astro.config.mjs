53ffa8bdcdcccf12eeb4c3760668551b2e463cf5
Jonathan Dunlap <jdunlap@outlook.com>
2024-08-06 18:58:50 -0400

    chore: switch back to Bun adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 // import react from "@astrojs/react"; // avoid using this for now
-// import bun from "@nurodev/astro-bun";
-import node from "@astrojs/node";
+import bun from "@nurodev/astro-bun";
+// import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import simpleStackQuery from "simple-stack-query";
 
@@ -11,7 +11,7 @@ export default defineConfig({
   // host: true,
   site: "https://app.thrivetogether.ai",
   output: "server",
-  adapter: node({
+  adapter: bun({
     host: true,
     mode: "standalone",
   }),
