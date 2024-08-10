35ef9ee4fd1d21d98244072b66c5280c4f464506
Jonathan Dunlap <jdunlap@outlook.com>
2024-08-10 12:44:57 -0400

    chore: add self to BB + delete last status message on post + debounce feed update for others

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,9 +1,14 @@
 // With `output: 'hybrid'` configured:
 // export const prerender = false;
 import type { APIRoute } from "astro";
+import { supabase } from "@/lib/supabase";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
-  cookies.delete('uid');
+  await supabase.auth.signOut();
+
+  cookies.delete("uid", { path: "/" });
+  cookies.delete("name", { path: "/" });
+  cookies.delete("location", { path: "/" });
   cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" });
   return redirect("/signin");
