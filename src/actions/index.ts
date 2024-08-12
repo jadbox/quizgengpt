8058321bb898300ce36ee1e6dbb86855bb7b5f32
Jonathan Dunlap <jdunlap@outlook.com>
2024-08-12 18:23:12 -0400

    chore: Update ActivitiesPage layout and button text

---
+++ src/actions/index.ts
@@ -0,0 +1,24 @@
+// src/actions/index.ts
+import { defineAction, z } from "astro:actions";
+import { setAstroSession } from "@/lib/supabase";
+
+export const server = {
+  resetGoals: defineAction({
+    accept: "form",
+    input: z.object({}),
+    handler: async ({}, context) => {
+      // get cookies from the context
+      const cookies = context.cookies;
+      const { supabase, data } = await setAstroSession(cookies);
+      const uid = data?.user?.id as string;
+
+      const { error } = await supabase
+        .from("goals")
+        .delete()
+        .eq("uid", uid)
+        .is("completed", null);
+      // call a mailing service, or store to a database
+      return { success: true };
+    },
+  }),
+};
