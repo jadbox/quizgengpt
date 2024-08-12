42d68779888b78c625b0482d46aad6d13fa51fd0
Jonathan Dunlap <jdunlap@outlook.com>
2024-08-12 19:52:25 -0400

    chore: cache supabase client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { AuthResponse, createClient } from "@supabase/supabase-js";
 import type { Database } from "../../supabase/supabase";
 import type { Tables } from "../../supabase/supabase";
 
@@ -39,6 +39,8 @@ export const supabase = createClient<Database>(
   }
 );
 
+// Cache last session
+let lastSession: AuthResponse;
 export const setAstroSession = async (cookies: any) => {
   const access_token = cookies.get("sb-access-token")?.value as string;
   const refresh_token = cookies.get("sb-refresh-token")?.value as string;
@@ -47,16 +49,19 @@ export const setAstroSession = async (cookies: any) => {
     return { supabase, data: null };
   }
 
-  const { data, error } = await supabase.auth.setSession({
-    access_token: access_token,
-    refresh_token: refresh_token,
-  });
+  if (!lastSession) {
+    var { data, error } = await supabase.auth.setSession({
+      access_token: access_token,
+      refresh_token: refresh_token,
+    });
 
-  if (error) {
-    console.error("Error:", error);
-    return { supabase, data: null, error };
-    // return new Response(error.message, { status: 500 });
-  }
+    if (error) {
+      console.error("Error:", error);
+      return { supabase, data: null, error };
+      // return new Response(error.message, { status: 500 });
+    }
+    if (typeof window !== "undefined") lastSession = data as any; // cache on browser
+  } else data = lastSession as any;
 
   if (!data || !data.session?.access_token) {
     console.error("No data or session found", data);
