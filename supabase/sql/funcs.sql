33ee12b928f3170c7b7e52b243b511ee44856a75
Jonathan Dunlap <jdunlap@outlook.com>
2024-08-12 10:37:49 -0400

    chore: update BB to say its weekly

--- supabase/sql/funcs.sql
+++ supabase/sql/funcs.sql
@@ -5,8 +5,9 @@ or replace function get_nearest_profiles (user_id uuid, limit_count integer) ret
   last_post timestamp with time zone,
   status text,
   distance float
-) as $$ BEGIN RETURN QUERY SELECT profiles.id, profiles.username, profiles.last_post, profiles.status, profiles.location, 
-ST_Distance(profiles.location, (SELECT p.location FROM profiles p WHERE p.id = user_id)) * 0.000621371192 AS distance 
-FROM profiles WHERE profiles.id != user_id AND profiles.last_post is not null
+) as $$ BEGIN RETURN QUERY SELECT profiles.id, profiles.username, profiles.last_post, profiles.status, ST_Distance(profiles.location, (SELECT p.location FROM profiles p WHERE p.id = user_id)) * 0.000621371192 AS distance 
+FROM profiles 
+WHERE profiles.last_post is not null
+  and profiles.last_post >= CURRENT_TIMESTAMP - INTERVAL '1 week'
 ORDER BY distance ASC, last_post DESC 
 LIMIT limit_count; END; $$ language plpgsql;
